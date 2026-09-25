716e8acc25d40bff549426c3c1681337809b1081
samiul122344-del <samiul122344@gamil.com>
2026-09-25 18:39:57 +0600

    Add out folder

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextMode} */
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
   },
 };
 
